87f149d7738af92554a9616cf817de45bc7e50b1
nivorako <nivo.rakoto@yahoo.fr>
2025-09-22 18:12:19 +0200

    use suspense to wrap Header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StyledComponentsRegistry from "./StyledComponentsRegistry";
@@ -42,7 +43,9 @@ export default function RootLayout({
             <body className={`${geistSans.variable} ${geistMono.variable}`}>
                 <StyledComponentsRegistry>
                     <Providers>
-                        <Header />
+                        <Suspense fallback={null}>
+                            <Header />
+                        </Suspense>
                         {children}
                         <Footer />
                     </Providers>
